c377fa7649f602cb40166c941ecf219e1150ae1d
kmcken <kmckenna@mymail.mines.edu>
2018-10-17 14:14:11 -0600

    v0.9.12.1 Updated read survey file to interpret units if present in the second line.

--- Utilities/readfromfile.py
+++ Utilities/readfromfile.py
@@ -1,4 +1,5 @@
-from Utilities import mylogging
+from Utilities import mylogging, unitconverter as units
+import csv
 import os
 
 try:
@@ -52,34 +53,27 @@ def complete_survey(file=root_path + '/Data/original_survey.csv', return_datafra
         return md, tvd, ns, ew, section, dls, build, turn, rugosity
 
 
-def survey(file=root_path + '/Data/original_survey.csv', return_dataframe=False):
+def survey(file=root_path + '/Data/original_survey.csv'):
     mylogging.runlog.info('Read: Survey File')
-    if __pandas is True:
-        try:
-            df = pd.read_csv(file, delimiter=',', header=0)
-        except FileNotFoundError:
-            mylogging.runlog.error('Read: {0} file not found.'.format(file))
-            raise FileNotFoundError
-        except Exception:
-            mylogging.runlog.error('Read: Error reading {0} file.'.format(file))
-            raise ValueError
-        if return_dataframe is True:
-            return df
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        lines = list(reader)
+    lines.pop(0)
 
-        md = df.MD.values
-        inc = df.Inc.values
-        azi = df.Azi.values
-        return md, inc, azi
-    else:
-        with open(file, 'r') as f:
-            reader = csv.reader(f)
-            lines = list(reader)
-        lines.pop(0)
+    md, inc, azi, unit_list = list(), list(), list(), None
+    try:
+        float(lines[0][0])
+    except ValueError:
+        unit_list = lines.pop(0)
 
-        md, inc, azi = list(), list(), list()
-        for line in lines:
-            md.append(float(line[0])), inc.append(float(line[1])), azi.append(float(line[2]))
-        return md, inc, azi
+    for line in lines:
+        md.append(float(line[0])), inc.append(float(line[1])), azi.append(float(line[2]))
+
+    if unit_list is not None:
+        md = units.from_to(md, unit_list[0], 'ft')
+        inc = units.from_to(inc, unit_list[1], 'dega')
+        azi = units.from_to(azi, unit_list[2], 'dega')
+    return md, inc, azi
 
 
 def error_model(file=root_path + '/Data/ErrorModel.txt'):
