v2.0.1 Fix bug that converted the angles to radians twice

SurveyCalculationMethods/Generic.py
@@ -25,9 +25,9 @@ class Survey:
         from Utilities import readfromfile as read
         md, inc, azi = read.survey(self.file)
 
-        self.MD = np.array(md)
-        self.Inc = np.array(inc)
-        self.Azi = np.array(azi)
+        self.md = np.array(md)
+        self.inc = np.array(inc)
+        self.azi = np.array(azi)
 
 
 class SurveyMethod:
@@ -37,9 +37,9 @@ class SurveyMethod:
         if target < 0:
             target = target + 360
         self.target = target
-        self.md = survey.MD
-        self.inc = survey.Inc
-        self.azi = survey.Azi
+        self.md = survey.md
+        self.inc = survey.inc
+        self.azi = survey.azi
         self.tvd = None
         self.north = None
         self.east = None

SurveyCalculationMethods/MinimumCurvature.py
@@ -14,19 +14,19 @@ def survey(md, inc, azi):
     :return: east, north, tvd, dls at survey points along the well.
     """
 
-    inc = [np.radians(ele) for ele in inc]
-    azi = [np.radians(ele) for ele in azi]
-
-    tvd, north, east, dls = list([0]), list([0]), list([0]), list([0])
-
+    tvd, north, east, dls, turn, build = list([0]), list([0]), list([0]), list([0]), list([0]), list([0])
     for i in range(1, len(md)):
-        next_point = next_pt(md[i] - md[i - 1], [inc[i - 1], inc[i]], [azi[i - 1], azi[i]])
-        tvd.append(tvd[i - 1] + next_point[0])
-        north.append(north[i - 1] + next_point[1])
-        east.append(east[i - 1] + next_point[2])
-        dls.append(next_point[3])
+        dm = md[i] - md[i - 1]
+        dv, dn, de, dls_current, turn_current, build_current \
+            = next_pt(dm, [inc[i - 1], inc[i]], [azi[i - 1], azi[i]])
+        tvd.append(tvd[i - 1] + dv)
+        north.append(north[i - 1] + dn)
+        east.append(east[i - 1] + de)
+        dls.append(dls_current)
+        turn.append(turn_current)
+        build.append(build_current)
 
-    return tvd, north, east, dls
+    return tvd, north, east, dls, turn, build
 
 
 def next_pt(delta_md, inc, azi, tolerance=1e-10):
@@ -51,8 +51,8 @@ def next_pt(delta_md, inc, azi, tolerance=1e-10):
     east = delta_md * ratio / 2.0 * (np.sin(inc[0]) * np.sin(azi[0]) + np.sin(inc[1]) * np.sin(azi[1]))
     tvd = delta_md * ratio / 2.0 * (np.cos(inc[0]) + np.cos(inc[1]))
     dls = np.degrees(beta) * 100.0 / delta_md
-    build = buildturn_rate(inc[0], inc[1], delta_md)
-    turn = buildturn_rate(azi[0], azi[1], delta_md)
+    build = np.degrees(buildturn_rate(inc[0], inc[1], delta_md)) * 100
+    turn = np.degrees(buildturn_rate(azi[0], azi[1], delta_md)) * 100
 
     return tvd, north, east, dls, build, turn
 
@@ -62,4 +62,4 @@ def buildturn_rate(angle0, angle1, delta_md):
     if delta_md == 0:
         return 0
 
-    return np.degrees(delta_angle) * 100 / delta_md
+    return delta_angle

SurveyCalculationMethods/MinimumCurvature2.py
@@ -14,18 +14,18 @@ def survey(md, inc, azi):
     :return: east, north, tvd, dls at survey points along the well.
     """
 
-    inc = [np.radians(ele) for ele in inc]
-    azi = [np.radians(ele) for ele in azi]
-
-    tvd, north, east, dls = list([0]), list([0]), list([0]), list([0])
+    tvd, north, east, dls, turn, build = list([0]), list([0]), list([0]), list([0]), list([0]), list([0])
     for i in range(1, len(md)):
-        dv, dn, de, dls_current = next_pt([md[i - 1], md[i]], [inc[i - 1], inc[i]], [azi[i - 1], azi[i]])
+        dv, dn, de, dls_current, turn_current, build_current \
+            = next_pt([md[i - 1], md[i]], [inc[i - 1], inc[i]], [azi[i - 1], azi[i]])
         tvd.append(tvd[i - 1] + dv)
         north.append(north[i - 1] + dn)
         east.append(east[i - 1] + de)
         dls.append(dls_current)
+        turn.append(turn_current)
+        build.append(build_current)
 
-    return tvd, north, east, dls
+    return tvd, north, east, dls, turn, build
 
 
 def next_pt(md2, inc2, azi2):

SurveyCalculationMethods/RadiiOfCurvature.py
@@ -14,18 +14,18 @@ def survey(md, inc, azi):
     :return: east, north, tvd, dls at survey points along the well.
     """
 
-    inc = [np.radians(ele) for ele in inc]
-    azi = [np.radians(ele) for ele in azi]
-
-    tvd, north, east, dls = list([0]), list([0]), list([0]), list([0])
+    tvd, north, east, dls, turn, build = list([0]), list([0]), list([0]), list([0]), list([0]), list([0])
     for i in range(1, len(md)):
-        dv, dn, de, dls_current = next_pt([md[i - 1], md[i]], [inc[i - 1], inc[i]], [azi[i - 1], azi[i]])
+        dv, dn, de, dls_current, turn_current, build_current \
+            = next_pt([md[i - 1], md[i]], [inc[i - 1], inc[i]], [azi[i - 1], azi[i]])
         tvd.append(tvd[i - 1] + dv)
         north.append(north[i - 1] + dn)
         east.append(east[i - 1] + de)
         dls.append(dls_current)
+        turn.append(turn_current)
+        build.append(build_current)
 
-    return tvd, north, east, dls
+    return tvd, north, east, dls, turn, build
 
 
 def next_pt(md2, inc2, azi2):
@@ -62,9 +62,9 @@ def next_pt(md2, inc2, azi2):
         dn = r_hor * (np.sin(azi2[1]) - np.sin(azi2[0]))
         de = r_hor * (np.cos(azi2[0]) - np.cos(azi2[1]))
 
-    dls = np.degrees(100 * np.sqrt((1 / r_vert) ** 2 + (1 / r_hor) ** 2))
-    build = buildturn_rate(inc2[0], inc2[1], dm)
-    turn = buildturn_rate(azi2[0], azi2[1], dm)
+    dls = np.degrees(np.sqrt((1 / r_vert) ** 2 + (1 / r_hor) ** 2)) * 100
+    build = np.degrees(buildturn_rate(inc2[0], inc2[1], dm)) * 100
+    turn = np.degrees(buildturn_rate(azi2[0], azi2[1], dm)) * 100
 
     return dv, dn, de, dls, build, turn
 
@@ -74,4 +74,4 @@ def buildturn_rate(angle0, angle1, delta_md):
     if delta_md == 0:
         return 0
 
-    return np.degrees(delta_angle) * 100 / delta_md
+    return np.degrees(delta_angle) / delta_md

SurveyCalculationMethods/trajectory.py
@@ -204,7 +204,7 @@ def minimum_curvature(survey_object, target=None, rnd=False):
     :param rnd: round to nearest 100th
     :type rnd: bool
     """
-    from SurveyCalculationMethods import MinimumCurvature2
+    from SurveyCalculationMethods import MinimumCurvature
 
     mylogging.runlog.info('Survey: Calculate survey for {0} using the Minimum Curvature method.'.format(survey_object.name))
     surv = Generic.SurveyMethod(survey_object, target)
@@ -212,7 +212,7 @@ def minimum_curvature(survey_object, target=None, rnd=False):
     surv.method = 'MinimumCurvature'
 
     surv.tvd, surv.north, surv.east, surv.dls,  surv.build,  surv.turn \
-        = MinimumCurvature2.survey(surv.md, np.radians(surv.inc), np.radians(surv.azi))
+        = MinimumCurvature.survey(surv.md, np.radians(surv.inc), np.radians(surv.azi))
 
     if surv.target is None:
         surv.target = closure_azimuth(surv.north[-1], surv.east[-1])

Utilities/writetofile.py
@@ -20,23 +20,23 @@ def object_csv(survey, rnd=False):
               'ft,dega,dega,ft,ft,ft,dega,ft,ft,dega/100ft,dega/100ft,dega/100ft,dega\n']
 
     if rnd is True:
-        md, inc, azi = np.round(survey.MD, 2), np.round(survey.Inc, 2), np.round(survey.Azi, 2)
-        tvd, ns, ew = np.round(survey.TVD, 2), np.round(survey.North, 2), np.round(survey.East, 2)
-        closure, departure = np.round(survey.Closure, 2), np.round(survey.Departure, 2)
-        section = np.round(survey.Section, 2)
-        dls, build, turn = np.round(survey.DLS, 2), np.round(survey.Build, 2), np.round(survey.Turn, 2)
+        md, inc, azi = np.round(survey.md, 2), np.round(survey.inc, 2), np.round(survey.azi, 2)
+        tvd, ns, ew = np.round(survey.tvd, 2), np.round(survey.north, 2), np.round(survey.east, 2)
+        closure, departure = np.round(survey.closure, 2), np.round(survey.departure, 2)
+        section = np.round(survey.section, 2)
+        dls, build, turn = np.round(survey.dls, 2), np.round(survey.build, 2), np.round(survey.turn, 2)
     else:
-        md, inc, azi, tvd, ns, ew = survey.MD, survey.Inc, survey.Azi, survey.TVD, survey.North, survey.East
-        closure, departure, section = survey.Closure, survey.Departure, survey.Section
-        dls, build, turn = survey.DLS, survey.Build, survey.Turn
+        md, inc, azi, tvd, ns, ew = survey.md, survey.inc, survey.azi, survey.tvd, survey.north, survey.east
+        closure, departure, section = survey.closure, survey.departure, survey.section
+        dls, build, turn = survey.dls, survey.build, survey.turn
 
     f = open(file, 'w')
     f.writelines(header)
-    for i in range(len(survey.MD)):
+    for i in range(len(survey.md)):
         line = [str(md[i]) + ',' + str(inc[i]) + ',' + str(azi[i]) + ',' + str(tvd[i]) + ','
                 + str(ns[i]) + ',' + str(ew[i]) + ',' + str(closure[i]) + ','
                 + str(departure[i]) + ',' + str(section[i]) + ',' + str(dls[i]) + ','
-                + str(build[i]) + ',' + str(turn[i]) + ',' + str(survey.Target) +'\n']
+                + str(build[i]) + ',' + str(turn[i]) + ',' + str(survey.target) + '\n']
         f.writelines(line)
     f.close()
 

main.py
@@ -26,117 +26,6 @@ def __init__():
     print("Now, lets get this thing on the hump. We got some flyin' to do.")
 
 
-def mcm(name='hrcg', target=None, adj=None):
-    mylogging.runlog.info('MCM: Calculate survey for {0}'.format(name))
-    file = __root_path + '/Data/' + name + '.csv'
-
-    md, inc, azi = read.survey(file)
-    survey = trajectory.calculate_survey(md, inc, azi, target, adj=adj, method='MCM')
-    write.survey_csv(survey, name=name, method='mcm')
-
-
-def asc(name='hrcg', target=None, adj=None):
-    mylogging.runlog.info('MCM: Calculate survey for {0}'.format(name))
-    file = __root_path + '/Data/' + name + '.csv'
-
-    md, inc, azi = read.survey(file)
-    survey = trajectory.calculate_survey(md, inc, azi, target, adj=adj, method='ASC')
-    write.survey_csv(survey, name=name, method='asc')
-
-
-def err(base='hrcg_mcm', name='hrcg100_asc'):
-    mylogging.runlog.info('Error: Calculate survey error.')
-    base_file = __root_path + '/Data/' + base + '.csv'
-    file = __root_path + '/Data/' + name + '.csv'
-
-    base_srv = read.complete_survey(file=base_file)
-    srv = read.complete_survey(file=file)
-    print('Average Error =', np.round(
-        trajectory.error(base_srv[0], srv[0], base_srv[1], srv[1], base_srv[2], srv[2], base_srv[3], srv[3]), 2), 'ft')
-
-
-def montecarlo_mcm(file='hrcg.csv'):
-    mylogging.runlog.info('Monte Carlo: Start Monte Carlo.')
-    file = __root_path + '/Data/' + file
-
-    montecarlo.survey_generation()
-    asc_list, asc_stats, mcm_list, mcm_stats = montecarlo.survey(100, 142, file)
-
-    if __plotEnabled is True:
-        fig1 = plt.figure()
-        wellplot.plot_vertical_section(asc_list, target_azimuth=142, size=len(asc_list), fig=fig1, color='b', label='ASC')
-        wellplot.plot_vertical_section(mcm_list, size=len(mcm_list), fig=fig1, color='k', label='MCM')
-        plt.gca().invert_yaxis()
-        plt.legend()
-
-        plt.figure()
-        wellplot.plot_horizontal_section(asc_list, size=len(asc_list), color='b', label='ASC')
-        wellplot.plot_horizontal_section(mcm_list, size=len(mcm_list), color='k', label='MCM')
-        plt.legend()
-
-        plt.figure()
-        wellplot.plot_dls(asc_list, color='b', label='ASC')
-        wellplot.plot_dls(mcm_list, color='k', label='MCM')
-        plt.gca().invert_yaxis()
-        plt.legend()
-
-        plt.show()
-
-
-def montecarlo_generation(file='hrcg.csv'):
-    pass
-
-
-def synthetic_well():
-    import Synthetic
-
-    alpha, delta, xi = 1000, 100, 100
-    tvd, ns, ew = Synthetic.true_well(delta, alpha, xi, size=10000)
-    md, inc, azi = Synthetic.survey(delta, alpha, xi, size=300)
-    print(azi)
-
-    Synthetic.write_well(tvd, ns, ew)
-    Synthetic.write_survey(md, inc, azi)
-
-    mcm('synthetic', target=355)
-    asc('synthetic', target=355)
-
-    mcmsyn = read.complete_survey(__root_path + '/Results/synthetic_mcm.csv', return_dataframe=True)
-    ascsyn = read.complete_survey(__root_path + '/Results/synthetic_asc.csv', return_dataframe=True)
-    syn = read.complete_survey(__root_path + '/Results/synthetic_true.csv', return_dataframe=True)
-
-    plt.figure()
-    plt.plot(syn.EW.values, syn.NS.values, label='Helical', color='k', alpha=0.5)
-    plt.scatter(mcmsyn.EW.values, mcmsyn.NS.values, label='MCM', color='r', s=2)
-    plt.scatter(ascsyn.EW.values, ascsyn.NS.values, label='ASC', color='b', s=2)
-    plt.xlabel('East/West')
-    plt.ylabel('North/South')
-    plt.legend(loc=1)
-    plt.gca().invert_yaxis()
-
-    plt.figure()
-    plt.plot(syn.EW.values, syn.TVD.values, label='Helical', color='k', alpha=0.5)
-    plt.scatter(mcmsyn.EW.values, mcmsyn.TVD.values, label='MCM', color='r', s=2)
-    plt.scatter(ascsyn.EW.values, ascsyn.TVD.values, label='ASC', color='b', s=2)
-    plt.xlabel('East/West')
-    plt.ylabel('True Vertical Depth')
-    # plt.legend(loc=1)
-    plt.gca().invert_yaxis()
-
-    # mpl.rcParams['legend.fontsize'] = 10
-    # fig = plt.figure()
-    # ax = fig.gca(projection='3d')
-    # ax.plot(ew, ns, tvd, label='Synthetic', color='k')
-    # ax.plot(mcmsyn.EW.values, mcmsyn.NS.values, mcmsyn.TVD.values, label='MCM', color='b')
-    # ax.plot(ascsyn.EW.values, ascsyn.NS.values, ascsyn.TVD.values, label='ASC', color='r')
-    # plt.xlabel('East/West')
-    # plt.ylabel('North/South')
-    # plt.gca().invert_zaxis()
-    # ax.legend()
-
-    plt.show()
-
-
 if __name__ == '__main__':
     __init__()
 
@@ -144,28 +33,26 @@ if __name__ == '__main__':
     survey = Generic.Survey('594survey')
     target = -158
 
-    # trajectory.tangential('594survey', target_azimuth=-158)
-    # trajectory.balanced_tangential('594survey', target_azimuth=-158)
+    trajectory.tangential(survey, target, rnd=True)
+    trajectory.balanced_tangential(survey, target, rnd=True)
     trajectory.average_angle(survey, target, rnd=True)
-    # trajectory.vector_average('594survey', target_azimuth=-158)
-    # trajectory.minimum_curvature('594survey', target_azimuth=-158)
-    # trajectory.minimum_curvature2('594survey', target_azimuth=-158)
-    # trajectory.radii_of_curvature('594survey', target_azimuth=-158)
-    # trajectory.advanced_splines('594survey', target_azimuth=-158)
-    import sys
-    sys.exit()
-    print(__root_path)
-    tan = read.complete_survey(__root_path + '/Results/594survey_Tangential.csv')
-    baltan = read.complete_survey(__root_path + '/Results/594survey_BalancedTangential.csv')
-    avgang = read.complete_survey(__root_path + '/Results/594survey_AverageAngle.csv')
-    vecavg = read.complete_survey(__root_path + '/Results/594survey_VectorAverage.csv')
-    mcm = read.complete_survey(__root_path + '/Results/594survey_MinimumCurvature2.csv')
-    radii = read.complete_survey(__root_path + '/Results/594survey_RadiiOfCurvature.csv')
-    asc = read.complete_survey(__root_path + '/Results/594survey_AdvancedSplines.csv')
+    trajectory.vector_average(survey, target, rnd=True)
+    trajectory.minimum_curvature(survey, target, rnd=True)
+    trajectory.radii_of_curvature(survey, target, rnd=True)
+    trajectory.advanced_splines(survey, target, rnd=True)
 
-    wellplot.plot_vertical_section(tan, target_azimuth=target, label='Tangential')
-    wellplot.plot_horizontal_section(tan, label='Tangential')
-    plt.show()
+    # print(__root_path)
+    # tan = read.complete_survey(__root_path + '/Results/594survey_Tangential.csv')
+    # baltan = read.complete_survey(__root_path + '/Results/594survey_BalancedTangential.csv')
+    # avgang = read.complete_survey(__root_path + '/Results/594survey_AverageAngle.csv')
+    # vecavg = read.complete_survey(__root_path + '/Results/594survey_VectorAverage.csv')
+    # mcm = read.complete_survey(__root_path + '/Results/594survey_MinimumCurvature.csv')
+    # radii = read.complete_survey(__root_path + '/Results/594survey_RadiusOfCurvature.csv')
+    # asc = read.complete_survey(__root_path + '/Results/594survey_AdvancedSplineCurve.csv')
+
+    # wellplot.plot_vertical_section(tan, target_azimuth=target, label='Tangential')
+    # wellplot.plot_horizontal_section(tan, label='Tangential')
+    # plt.show()
 
     print("We'll meet again.")
     mylogging.runlog.info("End: We'll meet again.")